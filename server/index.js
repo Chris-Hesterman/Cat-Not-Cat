29ff0c08e25756a541f2e89349526eea431e3b81
Chris Hesterman <chrishesterman14@gmail.com>
2020-05-26 12:37:02 -0700

    Get server up and running, connect to Watson Visual Recognition
    In progress. setting up env variables.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+require('custom-env').env();
+const VisualRecognitionV3 = require('obm-watson/visual-recognition/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const express = require('express');
+const bodyParser = require('body-parser');
+const items = require('../database-mongo');
 
-var app = express();
+const VisualRecognition = new VisualRecognitionV3({
+  version: '2018-03-19',
+  authenticator: new IamAuthenticator({
+    apikey: process.env.VISUAL_RECOGNITION_APIKEY
+  }),
+  url: process.env.VISUAL_RECOGNITION_URL
+});
+const app = express();
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
 app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
+  items.selectAll(function (err, data) {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -23,7 +31,6 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
-
